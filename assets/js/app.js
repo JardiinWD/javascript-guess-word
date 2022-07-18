494386e192b0fdd001caef04e75d96750ad94f28
Alessandro Pecorilla <Pecorillaalessandro@gmail.com>
2022-07-18 08:15:50 +0200

    Aggiunto funzione randomica e iniziato a iterare nel DOM

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,35 @@
 /* Verifica del collegamento File */
 /* alert("Ciao, sono il file App") */
+
+/* Questa è la costante degli input disabilitata. con questa pesco il div genitore */
+const inputs = document.querySelector(".inputs")
+
+
+/**
+ * Funzione per pescare oggetto randomico
+ * @ranObj => string che va a pescare, dall'array di parole una sezione a caso grazie a mathfloor
+ */
+function randomWord() {
+    /* Questo è l'oggetto randomico */
+    let ranObj = wordList[Math.floor(Math.random() * wordList.length)]
+    /* Questa è la sola parola */
+    let word = ranObj.word
+    console.log("Questa è la verifica dell'oggetto randomico");
+    console.log(ranObj);
+    console.log(`Questa è la verifica della parola randomica : ${word}`);
+    /* Ora creo la zona di input in relazione alla lunghezza delle parole randomiche */
+    /* Inizialmente vuota */
+    let html = "";
+    /* Avvio ciclo for */
+    for (let i = 0; i < word.length; i++) {
+        html = `<input type="text" disabled>`;
+    }
+    /* Verifico la variabile html */
+    console.log(`Questa è la verifica della variabile html : ${html}`);
+    /* Ora appendo il ciclo for alla mia const inputs */
+    inputs.innerHTML = html
+}
+
+/* La invoco per verificare il console log */
+randomWord();
+
---
+++ assets/js/word.js
@@ -0,0 +1,33 @@
+/* Questo è il file delle parole da Importare poi in App.js */
+/* E' un array di oggetti che, servirà come db, ma che contiene le risp
+per il gioco */
+const wordList = [
+    {
+        word: "cerca",
+        hint: "un'azione per trovare qualcosa"
+    },
+    {
+        word: "jpeg",
+        hint: "un formato immagine leggero"
+    },
+    {
+        word: "server",
+        hint: "relazionato ai computer o ai sistemi"
+    },
+    {
+        word: "python",
+        hint: "un linguaggio di programmazione"
+    },
+    {
+        word: "venere",
+        hint: "un pianeta del sistema solare"
+    },
+    {
+        word: "oro",
+        hint: "un metallo giallo prezioso"
+    },
+    {
+        word: "tromba",
+        hint: "uno strumento musicale"
+    },
+]
